1b285b574e68af219991abbfc0b2bd1aac977b05
ssankathali <muddamsetty.santhosh@gmail.com>
2021-05-19 19:53:04 -0400

    Season Display

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       lat: null,
+  //       errorMessage: "",
+  //     };
+  //   }
+
+  state = {
+    lat: null,
+    errorMessage: "",
+  };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
@@ -24,7 +32,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div className="">Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div className="">Loading</div>;
