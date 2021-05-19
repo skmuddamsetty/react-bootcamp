afbc90f7ccc1759a7c2fffa334ee724980f86bce
ssankathali <muddamsetty.santhosh@gmail.com>
2021-05-19 07:57:08 -0400

    added error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,16 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <div>
-      <SeasonDisplay />
-    </div>
-  );
-};
-
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="">
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}
 ReactDOM.render(<App />, document.querySelector("#root"));
