76f82cc6fac20a770083aec54081b32946e286ee
ssankathali <muddamsetty.santhosh@gmail.com>
2021-05-20 16:28:10 -0400

    added loader

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   //   constructor(props) {
@@ -35,7 +36,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div className="">Loading</div>;
+    return <Spinner />;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
