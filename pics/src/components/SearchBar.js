04649621e5156d0f08eb163a1f515fdc391f22fc
ssankathali <muddamsetty.santhosh@gmail.com>
2021-05-21 07:34:42 -0400

    styling for input box

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="">Image Search</label>
+            <input type="text" name="" id="" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
